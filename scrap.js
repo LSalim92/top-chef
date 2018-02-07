76eaa05b65af549b112e87075a8ed719b99f42bd
Salim <salim.laabi@hotmail.fr>
2018-02-07 22:49:21 +0100

    Retrieve all the starry restaurants from Michelin.fr (all pages)

--- scrap.js
+++ scrap.js
@@ -2,19 +2,21 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-request("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin", function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
+for(var i = 1; i<= 35; i++){
+  request("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-"+i, function(error, response, body) {
+    if(error) {
+      console.log("Error: " + error);
+    }
+    console.log("Status code: " + response.statusCode);
 
-  var $ = cheerio.load(body);
+    var $ = cheerio.load(body);
 
-  $('.poi-card-link').each(function() {
-     var link = $(this);
-     var text = $(this).find('.poi_card-display-title').text();
-     var href = link.attr('href');
+    $('.poi-card-link').each(function() {
+       var link = $(this);
+       var text = $(this).find('.poi_card-display-title').text();
+       var href = link.attr('href');
 
-     console.log(text + " -> " + "https://restaurant.michelin.fr" +href);
-   });
-});
+       console.log(text + " -> " + "https://restaurant.michelin.fr" +href);
+     });
+  });
+}
