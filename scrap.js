15585d5a7043350f77e35a99f2280d4d2d4985ae
Salim <salim.laabi@hotmail.fr>
2018-02-08 18:48:26 +0100

    Added storing in a list all the retrieved restaurant

--- scrap.js
+++ scrap.js
@@ -2,21 +2,35 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-for(var i = 1; i<= 35; i++){
-  request("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-"+i, function(error, response, body) {
-    if(error) {
-      console.log("Error: " + error);
-    }
-    console.log("Status code: " + response.statusCode);
+function scrapMichelin() {
+  var list_restaurant = [];
+  for(var i = 1; i<= 35; i++){
+    request("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-"+i, function(error, response, body) {
+      if(error) {
+        console.log("Error: " + error);
+      }
+      //console.log("Status code: " + response.statusCode);
 
-    var $ = cheerio.load(body);
+      var $ = cheerio.load(body);
 
-    $('.poi-card-link').each(function() {
-       var link = $(this);
-       var text = $(this).find('.poi_card-display-title').text();
-       var href = link.attr('href');
+      $('.poi-card-link').each(function() {
+         var link = $(this);
+         var name_restaurant = $(this).find('.poi_card-display-title').text();
+         var href_restaurant = link.attr('href');
 
-       console.log(text + " -> " + "https://restaurant.michelin.fr" +href);
-     });
-  });
+         var restaurant = {
+           name: name_restaurant,
+           href: "https://restaurant.michelin.fr" +href_restaurant
+         }
+
+         console.log(restaurant.name + " -> " + restaurant.href);
+
+         list_restaurant.push(restaurant);
+         console.log("list_restaurant.length = " + list_restaurant.length);
+       });
+    });
+  }
+  return list_restaurant;
 }
+
+var list = scrapMichelin();
